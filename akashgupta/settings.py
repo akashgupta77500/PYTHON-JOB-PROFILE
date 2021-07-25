d90010c820eaf82d2ae9446d3e2f81c4de34eae6
akashgupta77500 <39405315+akashgupta77500@users.noreply.github.com>
2021-07-25 18:06:04 +0530

    Update settings.py

--- akashgupta/settings.py
+++ akashgupta/settings.py
@@ -78,17 +78,11 @@ WSGI_APPLICATION = 'akashgupta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'akashgupta',
-        'USER': 'akash',
-        'PASSWORD': 'abcdefg@A2602199',
-        'HOST': '192.168.43.128',
-        'PORT': '3306',
-        'OPTIONS': {
-            'sql_mode': 'traditional',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
